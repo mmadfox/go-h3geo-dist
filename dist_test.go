ce59df1561627d1ba4d81154f492bb7c6e77e0e7
mmadfox <sergey.liskonog@gmail.com>
2021-12-27 18:57:27 +0300

    add test

--- dist_test.go
+++ dist_test.go
@@ -7,6 +7,41 @@ import (
 	"github.com/uber/h3-go/v3"
 )
 
+func TestDistributed_DynamicAddWithWeight(t *testing.T) {
+	dist := Default()
+	loop := 6
+	for n := 0; n < loop; n++ {
+		weight := 1
+		if n > 2 && n < 4 {
+			weight = n
+		}
+		addr := fmt.Sprintf("127.0.0.1:%d", n)
+		dist.AddWithWeight(addr, weight)
+		t.Logf("addNode=%s, weight=%d", addr, weight)
+
+		var found uint
+		stats := make(map[string]uint)
+		Iter(DefaultLevel, func(i uint, cell h3.H3Index) {
+			host, ok := dist.Lookup(uint64(cell))
+			if ok {
+				stats[host]++
+				found++
+			}
+		})
+		if have, want := found, Level4Area(); have != want {
+			t.Fatalf("found %d, want %d cells", have, want)
+		}
+		var have uint
+		for host, counter := range stats {
+			have += counter
+			t.Logf("host=%s, counter=%d", host, counter)
+		}
+		if found != have {
+			t.Fatalf("have %d, want %d", have, found)
+		}
+	}
+}
+
 func TestDistributed_DynamicAdd(t *testing.T) {
 	dist := Default()
 	loop := 6
